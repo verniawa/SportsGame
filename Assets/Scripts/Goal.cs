4063d21ad7b33d7730317a53d7c32d815a2d4ca8
William Vernia <verniawa@vcu.edu>
2021-11-03 21:24:08 -0400

    Added screen shake effect when a goal is scored

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -8,14 +8,21 @@ public class Goal : MonoBehaviour {
     int score = 0;
     public TextMeshProUGUI textMesh;
     ParticleSystem particleSystem;
+    Shake cameraShake;
     private void Start() {
         textMesh.text = score.ToString();
         particleSystem = GetComponent<ParticleSystem>();
+        cameraShake = Camera.main.GetComponent<Shake>();
     }
     public void scoreGoal(){
         score ++;
         textMesh.text = score.ToString();
         particleSystem.Play();
+        StartCoroutine(cameraShake.cameraShake(.5f, 1f));
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        ContactPoint2D contactPoint = collision.contacts[0];
+        
     }
 
 }
---
+++ Assets/Scripts/Shake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour {
+    public IEnumerator cameraShake(float duration, float magnitude){
+        Vector3 initialPosition = transform.localPosition;
+        float elapsed = 0f;
+        float trauma = magnitude;
+        float seed = Random.value * 50;
+        while (elapsed < duration){
+            trauma = Mathf.Lerp(magnitude, 0f, elapsed / duration);
+            float x = Mathf.Lerp(-1f, 1f, Mathf.PerlinNoise(seed + elapsed * 10f, 0f)) * trauma; 
+            float y = Mathf.Lerp(-1f, 1f, Mathf.PerlinNoise(0f, seed + elapsed * 10f)) * trauma;
+            
+            transform.localPosition = new Vector3(x, y, initialPosition.z);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.localPosition = initialPosition;
+    }
+}
